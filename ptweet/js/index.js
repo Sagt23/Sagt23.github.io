2755f21f640c8ad7841d0aa4f9eb461de52252db
Sagt23 <72633888+Sagt23@users.noreply.github.com>
2021-11-05 16:06:40 -0700

    index

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -1,5 +1,5 @@
 window.onload=()=>{
-    let usuario= JSON.parse(localStorage.getItem(usuario));
+    let usuario= JSON.parse(localStorage.getItem("usuario"));
     console.log(usuario);
     document.getElementById("usuario").innerHTML="Hola "+ usuario.nombre;
 };
